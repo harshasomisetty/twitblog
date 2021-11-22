381fd49dcf62d1a8f2ee58f4b04881ef8592d1c1
Harsha Somisetty <harshasomisetty7@gmail.com>
2021-11-22 16:45:38 -0500

    componented thread Page, added engagement metrics

--- frontend/src/routes/thread.jsx
+++ frontend/src/routes/thread.jsx
@@ -9,10 +9,6 @@ export default function Thread() {
   const [state, setState] = useState();
   const [isBusy, setBusy] = useState(true);
 
-  function getTweetLink(id, author) {
-    return "https://twitter.com/" + author + "/status/" + id;
-  }
-
   useEffect(() => {
     async function fetchData() {
       const url = "http://localhost:5000/thread/" + params.rootThread;
@@ -31,17 +27,47 @@ export default function Thread() {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   if (isBusy) return <p>loading</p>;
-
-  return (
-    <div>
-      <p>author: {state.threadData.author}</p>
-      <p>keywords: {state.threadData.keywords}</p>
-
-      {state.tweets.map((t) => (
-        <div key={t[1]}>
-          <a href={getTweetLink(t[1], state.threadData.author)}>{t[0]}</a>
-        </div>
-      ))}
-    </div>
-  );
+  else {
+    return (
+      <div>
+        <MetaData data={state.threadData} />
+        <ThreadText tweets={state.tweets} author={state.threadData.author} />
+      </div>
+    );
+  }
 }
+
+function capitalizeSent(str) {
+  const arr = str.replace("_", " ").split(" ");
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+  return arr.join(" ");
+}
+
+const MetaData = ({ data }) => (
+  <div>
+    <p>Author: {data.author}</p>
+    <p>Keywords: {data.keywords}</p>
+
+    {Object.keys(data.engagement).map((key) => (
+      <p>
+        {capitalizeSent(key)}: {data.engagement[key]}
+      </p>
+    ))}
+  </div>
+);
+
+function getTweetLink(id, author) {
+  return "https://twitter.com/" + author + "/status/" + id;
+}
+
+const ThreadText = ({ tweets, author }) => (
+  <div>
+    {tweets.map((t) => (
+      <div key={t[1]}>
+        <a href={getTweetLink(t[1], author)}>{t[0]}</a>
+      </div>
+    ))}
+  </div>
+);
