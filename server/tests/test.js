f0adadd4b8bab4545087eefaf214de86f03353de
Harsha Somisetty <harshasomisetty7@gmail.com>
2021-11-30 15:07:56 -0500

    fixed tests in dockerr run

--- server/tests/test.js
+++ server/tests/test.js
@@ -1,27 +1,11 @@
 var MongoClient = require("mongodb").MongoClient;
 require("dotenv").config({ path: ".env" });
 
-const fs = require("fs");
-
 var assert = require("assert");
 
 describe("Mongo Env Test", function () {
   it("Mongo should have env var and connect properly", function () {
     MongoClient.connect(process.env.MONGO_URI, function (err, client) {
-      console.log("dir");
-
-      fs.readFile("./.env", "utf8", function (err, data) {
-        if (err) throw err;
-        console.log("OK: ");
-        console.log(data);
-      });
-
-      fs.readFile("./testtxt", "utf8", function (err, data) {
-        if (err) throw err;
-        console.log("OK: ");
-        console.log(data);
-      });
-
       assert.equal(null, err);
     });
   });
